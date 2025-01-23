fa6d3a0ab5720ced70ae1f434baeb72e7148f6f2
kumawat2222 <129721544+kumawat2222@users.noreply.github.com>
2025-01-23 14:57:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 items =  {
          "719cd7fcaa6644698eab0ceec741d867":{
                                           "name": "Green mojhito",
-                                           "price": 200
+                                           "price": 500
                                             },
 
         "d786fd9073a54cf9aa2574662b1c4623":{
